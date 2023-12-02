44a373cf47d32b257362ac1e4a8a77d368374d47
aleksei <bestuzhev.aleksei.m@gmail.com>
2023-12-03 02:23:45 +0400

    feat: display extra info about user

--- src/features/profile/components/Person/Person.tsx
+++ src/features/profile/components/Person/Person.tsx
@@ -1,6 +1,7 @@
 import {AvatarModalContent} from '@/features/profile/components/Person/AvatarModalContent/AvatarModalContent.tsx';
 import {StatusModalContent} from '@/features/profile/components/Person/StatusModalContent/StatusModalContent.tsx';
 import anotherUserBcgWebp from '@/assets/images/profile-cover/another-user.webp';
+import {ExtraInfo} from "@/features/profile/components/ExtraInfo/ExtraInfo.tsx";
 import profileBcgWebp from '@/assets/images/profile-cover/profile-bcg.webp';
 import anotherUserBcg from '@/assets/images/profile-cover/another-user.jpg';
 import cls from '@/features/profile/components/Person/Person.module.scss';
@@ -12,75 +13,73 @@ import {Modal} from '@/components/Modal/Modal.tsx';
 import classNames from 'classnames';
 
 type PersonPropsType = PropsWithChildren & {
-   profile: UserProfileType,
-   status: string,
-   isMe: boolean,
+    profile: UserProfileType,
+    status: string,
+    isMe: boolean,
 };
 
 export const Person: FC<PersonPropsType> = ({isMe, status, profile, children}) => {
+    const {fullName, photos: {large: photo}} = profile;
 
-   const [photoIsOpen, setPhotoIsOpen] = useState(false);
+    const [photoIsOpen, setPhotoIsOpen] = useState(false);
 
-   const [statusEditMode, setStatusEditMode] = useState(false);
+    const [statusEditMode, setStatusEditMode] = useState(false);
 
-   const openPhotoModal = useCallback(() => {
-      setPhotoIsOpen(true);
-   }, []);
+    const openPhotoModal = useCallback(() => {
+        setPhotoIsOpen(true);
+    }, []);
 
-   const closePhotoModal = useCallback(() => {
-      setPhotoIsOpen(false);
-   }, []);
+    const closePhotoModal = useCallback(() => {
+        setPhotoIsOpen(false);
+    }, []);
 
-   const openStatusModal = useCallback(() => {
-      if (isMe) setStatusEditMode(true);
-   }, []);
+    const openStatusModal = useCallback(() => {
+        if (isMe) setStatusEditMode(true);
+    }, []);
 
-   const closeStatusModal = useCallback(() => {
-      setStatusEditMode(false);
-   }, []);
+    const closeStatusModal = useCallback(() => {
+        setStatusEditMode(false);
+    }, []);
 
-   const userName = profile.fullName;
+    const noStatusPlaceholder = isMe ? 'Enter status...' : ''
 
-   const photo = profile.photos.large;
+    const classes = {
+        status: classNames(cls.status, {[cls.pointer]: isMe})
+    }
 
-   const statusStyles = classNames(cls.status, {
-      [cls.pointer]: isMe
-   })
-
-   return (
-      <section>
-         <picture>
-            <source srcSet={isMe ? profileBcgWebp : anotherUserBcgWebp} type="image/webp" />
-            <img className={cls.header} src={isMe ? profileBcg : anotherUserBcg} alt={'Profile cover'}/>
-         </picture>
-         <div className={cls.person}>
-            <div className={cls.info}>
-               <Avatar size='12.5rem' photo={photo} border onClick={openPhotoModal} turnOffCursorPointer={!photo}/>
-               {
-                  photo && <Modal onClose={closePhotoModal} opened={photoIsOpen}>
-                       <AvatarModalContent photo={photo} name={userName}/>
-                   </Modal>
-               }
-               {
-                  isMe && <Modal onClose={closeStatusModal} opened={statusEditMode}>
-                       <StatusModalContent onClose={closeStatusModal}/>
-                   </Modal>
-               }
-               <div className={cls.about}>
-                  <p className={statusStyles} onClick={openStatusModal}>
-                     {
-                        status
-                           ? status
-                           : isMe
-                              ? 'Enter status...'
-                              : ''
-                     }
-                  </p>
-                  <h2 className={cls.name}>{userName}</h2>
-               </div>
+    return (
+        <section>
+            <picture>
+                <source srcSet={isMe ? profileBcgWebp : anotherUserBcgWebp} type="image/webp"/>
+                <img className={cls.header} src={isMe ? profileBcg : anotherUserBcg} alt={'Profile cover'}/>
+            </picture>
+            <div className={cls.person}>
+                <div className={cls.info}>
+                    <Avatar size='12.5rem' photo={photo} border onClick={openPhotoModal} turnOffCursorPointer={!photo}/>
+                    {
+                        photo && <Modal onClose={closePhotoModal} opened={photoIsOpen}>
+                            <AvatarModalContent photo={photo} name={fullName}/>
+                        </Modal>
+                    }
+                    {
+                        isMe && <Modal onClose={closeStatusModal} opened={statusEditMode}>
+                            <StatusModalContent onClose={closeStatusModal}/>
+                        </Modal>
+                    }
+                    <div className={cls.about}>
+                        <p className={classes.status} onClick={openStatusModal}>
+                            {
+                                status
+                                    ? status
+                                    : noStatusPlaceholder
+                            }
+                        </p>
+                        <h2 className={cls.name}>{fullName}</h2>
+                    </div>
+                </div>
+                {children}
             </div>
-            {children}
-         </div>
-      </section>
-   )
+            <ExtraInfo profile={profile} />
+        </section>
+    )
 }

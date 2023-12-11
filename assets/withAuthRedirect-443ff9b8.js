19c0efe0d669bea0875119d908db563dddbb3a1e
aleksei <bestuzhev.aleksei.m@gmail.com>
2023-12-11 17:17:18 +0400

    Updates

--- assets/withAuthRedirect-443ff9b8.js
+++
@@ -1,1 +0,0 @@
-import{k as r,E as o,j as t,$ as a}from"./index-555df8c0.js";const n=s=>e=>r(o)?t.jsx(s,{...e}):t.jsx(a,{to:"/login"});export{n as w};
---
+++ assets/withAuthRedirect-9177b18f.js
@@ -0,0 +1,1 @@
+import{k as r,F as o,j as t,a0 as a,m as u}from"./index-7340c2ec.js";const c=s=>e=>r(o)?t.jsx(s,{...e}):t.jsx(a,{to:u.login});export{c as w};
